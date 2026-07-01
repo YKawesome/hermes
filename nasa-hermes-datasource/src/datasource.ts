b83bd0554e0600f395aeebe0099a480ad2d42aec
Yousef Khan <theyousefkhan@gmail.com>
2026-07-01 14:31:15 -0700

    Hide source when selected (frontend)

--- nasa-hermes-datasource/src/datasource.ts
+++ nasa-hermes-datasource/src/datasource.ts
@@ -1,10 +1,25 @@
-import { DataSourceInstanceSettings, CoreApp, ScopedVars } from '@grafana/data';
+import { DataQueryRequest, DataSourceInstanceSettings, CoreApp, ScopedVars } from '@grafana/data';
 import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
+import { map } from 'rxjs/operators';
 import { MyQuery, MyDataSourceOptions, DEFAULT_QUERY } from './types';
 
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+  }
+
+  query(request: DataQueryRequest<MyQuery>) {
+    return super.query(request).pipe(
+      map((response) => {
+        for (const result of response.data) {
+          const query = request.targets.find((t) => t.refId === result.refId);
+          if (query?.queryType === 'events' && query.source) {
+            result.fields = result.fields.filter((f: { name: string }) => f.name !== 'source');
+          }
+        }
+        return response;
+      })
+    );
   }
 
   getDefaultQuery(_: CoreApp): Partial<MyQuery> {
