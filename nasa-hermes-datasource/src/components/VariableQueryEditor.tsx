c4ed6d558cf768c99ed05212386f9668dcd331f4
Yousef Khan <theyousefkhan@gmail.com>
2026-07-20 14:21:14 -0700

    Fit default bug

--- nasa-hermes-datasource/src/components/VariableQueryEditor.tsx
+++ nasa-hermes-datasource/src/components/VariableQueryEditor.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Combobox, ComboboxOption, InlineField } from '@grafana/ui';
 import { VariableQuery } from '../datasource';
+
+const DEFAULT_QUERY_TYPE = 'channels';
 
 const QUERY_OPTIONS: ComboboxOption[] = [
   { label: 'Components', value: 'components' },
@@ -15,11 +17,17 @@ interface Props {
 }
 
 export function VariableQueryEditor({ query, onChange }: Props) {
+  useEffect(() => {
+    if (!query.queryType) {
+      onChange({ ...query, queryType: DEFAULT_QUERY_TYPE }, DEFAULT_QUERY_TYPE);
+    }
+  }, [query, onChange]);
+
   return (
     <InlineField label="Query type" labelWidth={20}>
       <Combobox
         options={QUERY_OPTIONS}
-        value={query.queryType || 'channels'}
+        value={query.queryType || DEFAULT_QUERY_TYPE}
         onChange={(opt) => {
           if (opt?.value) {
             onChange({ ...query, queryType: opt.value }, opt.value);
